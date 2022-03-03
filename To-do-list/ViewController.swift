0b1536695eec63dc33d27dcc980795b39e1c4c05
inetamagone <99735096+inetamagone@users.noreply.github.com>
2022-03-03 10:51:48 +0200

    TableView prepared
    TableView prepared

--- To-do-list/ViewController.swift
+++ To-do-list/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.rowHeight = 60
+
     }
 
+    @IBAction func addTask(_ sender: Any) {
+    }
+    
+    @IBAction func deleteTask(_ sender: Any) {
+    }
+    
+}
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
 }
 
